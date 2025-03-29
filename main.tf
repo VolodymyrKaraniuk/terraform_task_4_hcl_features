62f0c534578c953be36b0e7b9cfa2d5dbf8b2d86
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-03-29 13:40:44 +0100

    Solution

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm-${count.index}"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main[local.network_interface_names[0]].id]
+  network_interface_ids = [azurerm_network_interface.main[local.network_interface_names[count.index]].id]
   vm_size               = "Standard_DS1_v2"
   lifecycle {
     prevent_destroy = true
